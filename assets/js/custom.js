54f44b67b3a589ee10db51aa5015f03556744038
Brian Martin Ortiz <brianmartinortiz@gmail.com>
2023-12-02 01:20:20 -0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -48,8 +48,8 @@
 		var typed = new Typed('#typed', {
 		    stringsElement: '#typed-strings',
 		    typeSpeed: 75,
-		    backSpeed: 20,
-		    startDelay: 1000,
+		    backSpeed: 75,
+		    startDelay: 3000,
 		    loop: true,
 		    loopCount: Infinity
 		});
